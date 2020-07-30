4c41bcee0966cdc4b01d138a3ba95e895d23cf08
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-30 08:34:16 -0300

    test: ensure SignUpController returns an error if no email is provided

--- src/presentation/controllers/SignUp.spec.ts
+++ src/presentation/controllers/SignUp.spec.ts
@@ -16,4 +16,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const password = faker.internet.password()
+    const httpRequest = {
+      body: {
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        password: password,
+        passwordConfirmation: password
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
