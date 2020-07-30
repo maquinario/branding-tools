9e892ff2cbd3a5318ed8d7d4a4a33889e395a3c9
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-30 08:51:12 -0300

    test: ensure SignUpController returns 400 if no password confirmation is provided

--- src/presentation/controllers/SignUp.spec.ts
+++ src/presentation/controllers/SignUp.spec.ts
@@ -33,16 +33,28 @@ describe('SignUp Controller', () => {
   })
   test('Should return 400 if no password is provided', () => {
     const sut = new SignUpController()
-    const password = faker.internet.password()
     const httpRequest = {
       body: {
         name: `${faker.name.firstName()} ${faker.name.lastName()}`,
         email: faker.internet.email(),
-        passwordConfirmation: password
+        passwordConfirmation: faker.internet.password()
       }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+  test('Should return 400 if no password confirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        email: faker.internet.email(),
+        password: faker.internet.password()
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 })
