25f1a93f86ca5666f2c852de88d49ce7afdac6fb
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-01 16:52:11 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/AccountRepository/Account.spec.ts
@@ -0,0 +1,30 @@
+import faker from 'faker'
+import { MongoHelper } from '../helpers/MongoHelper'
+
+import { AccountMongoRepository } from './Account'
+
+describe('Accoung Mongo Repository', () => {
+  const accountData = {
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  }
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add(accountData)
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(accountData.name)
+    expect(account.email).toBe(accountData.email)
+    expect(account.password).toBe(accountData.password)
+  })
+})
