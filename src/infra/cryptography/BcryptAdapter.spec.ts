1520415e2b5e2bca56eaf41892512851eb1fea44
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-01 13:30:13 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/cryptography/BcryptAdapter.spec.ts
+++ src/infra/cryptography/BcryptAdapter.spec.ts
@@ -2,6 +2,13 @@ import faker from 'faker'
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './BcryptAdapter'
 
+const hashString = faker.random.alphaNumeric(36)
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve(hashString))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const salt = 12
@@ -10,5 +17,13 @@ describe('Bcrypt Adapter', () => {
     const password = faker.internet.password()
     await sut.encrypt(password)
     expect(hashSpy).toHaveBeenCalledWith(password, salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const password = faker.internet.password()
+    const hash = await sut.encrypt(password)
+    expect(hash).toBe(hashString)
   })
 })
