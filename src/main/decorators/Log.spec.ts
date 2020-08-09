44ce70ba52d8e2c74c9c35c8d247df2710e68cab
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-09 16:54:39 -0300

    refactor: move data layer interfaces to folders

--- src/main/decorators/Log.spec.ts
+++ src/main/decorators/Log.spec.ts
@@ -2,7 +2,7 @@ import faker from 'faker'
 import { LogControllerDecorator } from './Log'
 import { HttpRequest, HttpResponse, Controller } from '../../presentation/protocols'
 import { serverError, ok } from '../../presentation/helpers/Http/HttpHelper'
-import { LogErrorRepository } from '../../data/protocols/LogErrorRepository'
+import { LogErrorRepository } from '../../data/protocols/db/LogErrorRepository'
 import { AccountModel } from '../../domain/models/Account'
 
 interface SutTypes{
--- src/main/decorators/Log.ts
+++ src/main/decorators/Log.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Controller } from '../../presentation/protocols'
-import { LogErrorRepository } from '../../data/protocols/LogErrorRepository'
+import { LogErrorRepository } from '../../data/protocols/db/LogErrorRepository'
 
 export class LogControllerDecorator implements Controller {
   private readonly controller: Controller
