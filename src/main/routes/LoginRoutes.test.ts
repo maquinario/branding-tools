f85b0f3ad99f3a4514fb7c0dcb519c354c97d055
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-11 18:53:02 -0300

    refactor: rename SignUpRoutes to LoginRoutes

--- src/main/routes/LoginRoutes.test.ts
+++ src/main/routes/LoginRoutes.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest'
 import app from '../config/App'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/MongoHelper'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -16,17 +16,18 @@ describe('SignUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-
-  test('Should return an account on success', async () => {
-    const password = faker.internet.password()
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: faker.name.firstName(),
-        email: faker.internet.email(),
-        password,
-        passwordConfirmation: password
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      const password = faker.internet.password()
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: faker.name.firstName(),
+          email: faker.internet.email(),
+          password,
+          passwordConfirmation: password
+        })
+        .expect(200)
+    })
   })
 })
