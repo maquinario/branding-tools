9bd57db25413befa02edb26ef9596787603798d7
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-13 12:31:40 -0300

    fix: use host in heroku

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,12 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/MongoHelper'
 import env from './config/Env'
 
+const { port, host } = env
+
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/App')).default
-    app.listen(env.port, env.host, () => {
+    app.listen(Number(port), host, () => {
       console.log(`⚙ Server running at http://localhost:${env.port}!`)
     })
   })
