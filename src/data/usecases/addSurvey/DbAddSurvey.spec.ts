5298dfeed39d5d938743c94eb1d68bc7626937cd
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-22 20:58:46 -0300

    test: ensure DbAddSurvey throws if AddSurveyRepository throws

--- src/data/usecases/addSurvey/DbAddSurvey.spec.ts
+++ src/data/usecases/addSurvey/DbAddSurvey.spec.ts
@@ -37,4 +37,16 @@ describe('DbAddSurvey Usecase', () => {
     await sut.add(surveyData)
     expect(addSpy).toHaveBeenCalledWith(surveyData)
   })
+
+  test('Should throw if AddSurveyRepository throws', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    jest
+      .spyOn(addSurveyRepositoryStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+    const accountData = makeFakeSurveyData()
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
