ebbeb85ea4920a4ef97539515b4a9cedd8c0e91f
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-29 13:37:38 -0300

    fix: change httpRequest header error

--- src/main/adapters/ExpressMiddlewareAdapter.ts
+++ src/main/adapters/ExpressMiddlewareAdapter.ts
@@ -4,10 +4,10 @@ import { HttpRequest, Middleware } from '../../presentation/protocols'
 export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const httpRequest: HttpRequest = {
-      body: req.headers
+      headers: req.headers
     }
     const httpResponse = await middleware.handle(httpRequest)
-    if (httpResponse.statusCode === 200 || httpResponse.statusCode <= 299) {
+    if (httpResponse.statusCode === 200) {
       Object.assign(req, httpResponse.body)
       next()
     } else {
